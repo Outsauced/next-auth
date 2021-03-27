ed75ac236dd3151f4ed4780e7a807c3bab595877
dackers86 <ackers86@hotmail.com>
2021-03-27 14:17:18 +0000

    fixed protcol

--- src/lib/get-current-url.js
+++ src/lib/get-current-url.js
@@ -1,16 +1,10 @@
-export const getServerUrl = req => {
-    const protocol = req.headers.host.includes(["http", "https"])
-      ? new URL(req.headers.host).protocol
-      : "http";
-  
+  export const getServerUrl = req => {
+    const protocol = new URL(req.headers.host).protocol || "http";
     return `${protocol}://${req.headers.host}/api/auth`;
   };
   
   export const getClientUrl = () => {
-    const protocol = window.location.host.includes(["http", "https"])
-      ? new URL(req.headers.host).protocol
-      : "http";
-  
+    const protocol = new URL(window.location.host).protocol || "http";
     return `${protocol}://${window.location.host}/api/auth`;
   };
   
