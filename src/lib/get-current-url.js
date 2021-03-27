2d92b72f4fdff16d5a3d5feb833bd5bded82aefd
dackers86 <ackers86@hotmail.com>
2021-03-27 14:46:20 +0000

    Updated client protocol

--- src/lib/get-current-url.js
+++ src/lib/get-current-url.js
@@ -4,7 +4,6 @@
   };
   
   export const getClientUrl = () => {
-    const protocol = new URL(window.location.host).protocol || "http";
-    return `${protocol}://${window.location.host}/api/auth`;
+    return `${protocol}://${window.location.origin}/api/auth`;
   };
   
