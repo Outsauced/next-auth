42d4ebd88fd17bd41e178528c9275b8c4426a4aa
dackers86 <ackers86@hotmail.com>
2021-04-17 11:53:20 +0100

    client updates

--- src/lib/get-current-url.js
+++ src/lib/get-current-url.js
@@ -1,9 +1,23 @@
-  export const getServerUrl = req => {
-    var protocol = req.headers.host.indexOf("http://")==0?"http://":"https://";
-    return `${protocol}${req.headers.host}/api/auth`;
-  };
-  
-  export const getClientUrl = () => {
-    return `${window.location.origin}/api/auth`;
-  };
-  
+import logger from "./logger";
+
+export const getServerUrl = req => {
+  if (req.headers.host.indexOf("http://")) {
+    logger.warn("Https is required for signing in!");
+  }
+
+  var protocol =
+    req.headers.host.indexOf("https://") == 0 ? "https://" : "http://";
+
+  console.log(
+    "Running Serversssss >>>",
+    req.headers.host,
+    `${protocol}${req.headers.host}/api/auth`
+  );
+
+  return `${protocol}${req.headers.host}/api/auth`;
+};
+
+export const getClientUrl = () => {
+  console.log("Running Client >>>>", `${window.location.origin}/api/auth`);
+  return `${window.location.origin}/api/auth`;
+};
