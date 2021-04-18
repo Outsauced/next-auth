431bb4cf0be6aa16014023373fe9e91802162c94
dackers86 <ackers86@hotmail.com>
2021-04-18 09:42:42 +0100

    client url update & version bump

--- src/lib/get-current-url.js
+++ src/lib/get-current-url.js
@@ -2,20 +2,20 @@ import logger from "./logger";
 
 export const getServerUrl = req => {
   // Default to HTTPS if no protocol explictly specified
-  const protocol = req.protocol || "https";
+  const protocol = (req.headers.origin || req.headers.referer || "").startsWith(
+    "http:"
+  )
+    ? "http"
+    : "https";
 
   console.log(
-    "Running Serversssss >>>",
-    protocol + "://" + req.get("host") + req.originalUrl
+    "Running Server >>>",
+    `${protocol}://${req.headers.host}/api/auth`
   );
 
-  return protocol + "://" + req.get("host") + req.originalUrl;
+  return `${protocol}://${req.headers.host}/api/auth`;
 };
 
 export const getClientUrl = () => {
-  return window.location.href;
-  // const protocol = window.location.protocol || "https";
-
-  // console.log("Running Client >>>>", `${window.location.origin}/api/auth`);
-  // return `${protocol}://${window.location.origin}/api/auth`;
+  return `${window.location.origin}/api/auth`;
 };
